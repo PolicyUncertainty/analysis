2316e3b6253a931e22ba2e8c93cd3304c0d786a6
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-01-16 16:20:58 -0500

    Black and some things.

--- MASTER_prepare_structural_model.py
+++ MASTER_prepare_structural_model.py
@@ -9,30 +9,33 @@
 # dependencies: original data saved on local machines, functions for steps 0-3 in src folder
 # output: data (decisions and outside_parameters) saved in output folder
 
-
 # %%
 # Step 0: Set paths and parameters
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
+USER = "max"
 
 # Set file paths
-paths_dict = {
-    # SOEP Core and SOEP RV are saved locally
-    "soep_c38": "C:/Users/bruno/papers/soep/soep38",
-    "soep_rv": "C:/Users/bruno/papers/soep/soep_rv",
-    #"soep_c38": "/home/maxbl/Uni/pol_uncetainty/data/soep38",
-    #"soep_rv": "/home/maxbl/Uni/pol_uncetainty/data/soep_rv",
-    
-    # SOEP IS (our own questions) is saved on the server
-    "soep_is": "../data/dataset_main_SOEP_IS.dta"
-}
-
-# Set recurring parameters 
+if USER == "bruno":
+    paths_dict = {
+        # SOEP Core and SOEP RV are saved locally
+        "soep_c38": "C:/Users/bruno/papers/soep/soep38",
+        "soep_rv": "C:/Users/bruno/papers/soep/soep_rv",
+        # SOEP IS (our own questions) is saved on the server
+        "soep_is": "../data/dataset_main_SOEP_IS.dta",
+    }
+elif USER == "max":
+    paths_dict = {
+        "soep_c38": "/home/maxbl/Uni/pol_uncetainty/data/soep38",
+        "soep_rv": "/home/maxbl/Uni/pol_uncetainty/data/soep_rv",
+        "soep_is": "/home/maxbl/Uni/pol_uncetainty/data/dataset_main_SOEP_IS.dta",
+    }
+# Set recurring parameters
 min_SRA = 65
 min_ret_age = min_SRA - 4
 max_ret_age = 72
-exp_cap = 40 # maximum number of periods of exp accumulation
-start_year = 2010 # start year of estimation sample
-end_year = 2021 # end year of estimation sample
+exp_cap = 40  # maximum number of periods of exp accumulation
+start_year = 2010  # start year of estimation sample
+end_year = 2021  # end year of estimation sample
 
 # Set options for data preparation
 data_options = {
@@ -41,7 +44,7 @@ data_options = {
     "start_age": 25,
     "min_ret_age": min_ret_age,
     "max_ret_age": max_ret_age,
-    "exp_cap": exp_cap
+    "exp_cap": exp_cap,
 }
 
 # Set options for estimation of wage equation parameters
@@ -49,7 +52,7 @@ wage_eq_options = {
     "start_year": start_year,
     "end_year": end_year,
     "exp_cap": exp_cap,
-    "wage_dist_truncation_percentiles": [0.01, 0.99]
+    "wage_dist_truncation_percentiles": [0.01, 0.99],
 }
 
 # Set options for estimation of policy expectation process parameters
@@ -69,9 +72,10 @@ policy_expectation_options = {
 
 # %%
 # Step 1: Get choice and state variables from SOEP core and SOEP RV VSKT
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 
 from src.gather_decision_data import gather_decision_data
+
 dec_data = gather_decision_data(paths_dict, data_options, load_data=False)
 # Add checks on our model choice sets. I.e. no retirement before min_ret_age,
 # no working/unemployment after max_ret_age.
@@ -81,15 +85,19 @@ dec_data = gather_decision_data(paths_dict, data_options, load_data=False)
 
 # %%
 # Step 2: Estimates wage equation parameters
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 from src.wage_equation import estimate_wage_parameters
+
 wage_params = estimate_wage_parameters(paths_dict, wage_eq_options)
 
 # %%
 # Step 3: Estimates policy expectation process parameters
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 from src.ret_age_expectations import estimate_policy_expectation_parameters
-policy_expectation_params = estimate_policy_expectation_parameters(paths_dict, policy_expectation_options)
+
+policy_expectation_params = estimate_policy_expectation_parameters(
+    paths_dict, policy_expectation_options
+)
 # %%
 
 # %%
--- src/test_decision_data.py
+++ src/test_decision_data.py
@@ -1,68 +1,93 @@
 import pytest
 from gather_decision_data import gather_decision_data
 
+LOAD_SAVED_DATA = True
+USER = "bruno"
+
+
 # Define paths_dict as a fixture
 @pytest.fixture
-def paths_dict(): 
-    return {
-    # SOEP Core and SOEP RV are saved locally
-    "soep_c38": "C:/Users/bruno/papers/soep/soep38",
-    "soep_rv": "C:/Users/bruno/papers/soep/soep_rv",
-    #"soep_c38": "/home/maxbl/Uni/pol_uncetainty/data/soep38",
-    #"soep_rv": "/home/maxbl/Uni/pol_uncetainty/data/soep_rv",
-    
-    # SOEP IS (our own questions) is saved on the server
-    "soep_is": "../data/dataset_main_SOEP_IS.dta"
-    }
+def paths_dict():
+    if USER == "bruno":
+        return {
+            # SOEP Core and SOEP RV are saved locally
+            "soep_c38": "C:/Users/bruno/papers/soep/soep38",
+            "soep_rv": "C:/Users/bruno/papers/soep/soep_rv",
+        }
+    elif USER == "max":
+        return {
+            "soep_c38": "/home/maxbl/Uni/pol_uncetainty/data/soep38",
+            "soep_rv": "/home/maxbl/Uni/pol_uncetainty/data/soep_rv",
+        }
+
 
 # Define data_options as a fixture
 @pytest.fixture
-def data_options(): 
+def data_options():
     min_SRA = 65
     min_ret_age = min_SRA - 4
     max_ret_age = 72
-    exp_cap = 40 # maximum number of periods of exp accumulation
-    start_year = 2010 # start year of estimation sample
-    end_year = 2021 # end year of estimation sample
+    exp_cap = 40  # maximum number of periods of exp accumulation
+    start_year = 2010  # start year of estimation sample
+    end_year = 2021  # end year of estimation sample
     return {
         "start_year": start_year,
         "end_year": end_year,
         "start_age": 25,
         "min_ret_age": min_ret_age,
         "max_ret_age": max_ret_age,
-        "exp_cap": exp_cap
-        }
+        "exp_cap": exp_cap,
+    }
 
 
 # These function tests the decision data for consistency (cf. model state space sparsity condition).
 
-def test_decision_data_no_missing_values(paths_dict,data_options, load_data=True):
+
+def test_decision_data_no_missing_values(
+    paths_dict, data_options, load_data=LOAD_SAVED_DATA
+):
     """This functions asserts that there are no missing values for any states"""
     dec_dat = gather_decision_data(paths_dict, data_options, load_data=load_data)
     assert dec_dat["choice"].isna().sum() == 0
     assert dec_dat["period"].isna().sum() == 0
     assert dec_dat["lagged_choice"].isna().sum() == 0
     assert dec_dat["policy_state"].isna().sum() == 0
-    #assert dec_dat["retirement_age_id"].isna().sum() == 0  // this is the only exception because it is irrelevant for matched decisions
+    # assert dec_dat["retirement_age_id"].isna().sum() == 0  // this is the only exception because it is irrelevant for matched decisions
     assert dec_dat["experience"].isna().sum() == 0
 
-def test_decision_data_no_ret_before_min_ret_age(paths_dict,data_options, load_data=True):
-    """This functions asserts that nobody is retired before min_ret_age"""
-    dec_dat = gather_decision_data(paths_dict,data_options, load_data=load_data)
-    assert dec_dat.loc[dec_dat["choice"] == 2, "period"].min() + data_options["start_age"] >= data_options["min_ret_age"] 
 
-def test_decision_data_no_work_after_max_ret_age(paths_dict,data_options, load_data=True):
+def test_decision_data_no_ret_before_min_ret_age(
+    paths_dict, data_options, load_data=LOAD_SAVED_DATA
+):
+    """This functions asserts that nobody is retired before min_ret_age"""
+    dec_dat = gather_decision_data(paths_dict, data_options, load_data=load_data)
+    assert (
+        dec_dat.loc[dec_dat["choice"] == 2, "period"].min() + data_options["start_age"]
+        >= data_options["min_ret_age"]
+    )
+
+
+def test_decision_data_no_work_after_max_ret_age(
+    paths_dict, data_options, load_data=LOAD_SAVED_DATA
+):
     """This functions asserts that there are no working after max_ret_age"""
     dec_dat = gather_decision_data(paths_dict, data_options, load_data=load_data)
-    assert dec_dat.loc[dec_dat["choice"] == 1, "period"].max() + data_options["start_age"] <= data_options["max_ret_age"]
+    assert (
+        dec_dat.loc[dec_dat["choice"] == 1, "period"].max() + data_options["start_age"]
+        <= data_options["max_ret_age"]
+    )
 
-def test_decision_data_exp_cap(paths_dict,data_options, load_data=True):
+
+def test_decision_data_exp_cap(paths_dict, data_options, load_data=LOAD_SAVED_DATA):
     """This functions asserts that experience is smaller or equal to age and exp_cap"""
     dec_dat = gather_decision_data(paths_dict, data_options, load_data=load_data)
     assert dec_dat["experience"].max() <= data_options["exp_cap"]
     assert dec_dat["experience"].max() <= dec_dat["period"].max()
 
-def test_decision_data_retirement_is_absorbing(paths_dict,data_options, load_data=True):
+
+def test_decision_data_retirement_is_absorbing(
+    paths_dict, data_options, load_data=LOAD_SAVED_DATA
+):
     """This functions asserts that retirement is absorbing"""
     dec_dat = gather_decision_data(paths_dict, data_options, load_data=load_data)
     assert dec_dat.loc[dec_dat["lagged_choice"] == 2, "choice"].unique() == 2

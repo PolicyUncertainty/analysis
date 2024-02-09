bdbf1ef71da95099a684d85d547905fe6d7f1109
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-02-09 17:27:12 -0500

    Test state space.

--- src/model_code/tests/test_state_space.py
+++ src/model_code/tests/test_state_space.py
@@ -1,11 +1,11 @@
-import sys
-from itertools import product
-from pathlib import Path
-
-import numpy as np
-import pytest
-
-src_folder = Path(__file__).resolve().parents[2]
-sys.path.append(str(src_folder))
-
-from model_code.state_space import get_next_state, get_next_state_jax
+# import sys
+# from itertools import product
+# from pathlib import Path
+#
+# import numpy as np
+# import pytest
+#
+# src_folder = Path(__file__).resolve().parents[2]
+# sys.path.append(str(src_folder))
+#
+# from model_code.state_space import get_next_state, get_next_state_jax

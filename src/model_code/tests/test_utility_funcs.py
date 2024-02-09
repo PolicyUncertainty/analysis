4d05c1d91886f73c4f808df3eb33463be6ab69d8
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-02-09 15:33:01 -0500

    Grids

--- src/model_code/tests/test_utility_funcs.py
+++ src/model_code/tests/test_utility_funcs.py
@@ -3,27 +3,36 @@ from pathlib import Path
 
 src_folder = Path(__file__).resolve().parents[2]
 sys.path.append(str(src_folder))
-
+import pytest
 from model_code.utility_functions import utility_func, inverse_marginal, marg_utility
 import numpy as np
+from itertools import product
+
+MU_GRID = np.linspace(0.1, 0.9, 3)
+CONSUMPTION_GRID = np.linspace(10, 100, 5)
+DISUTIL_UNEMPLOYED_GRID = np.linspace(0.1, 0.9, 3)
+DISUTIL_WORK_GRID = np.linspace(0.1, 0.9, 3)
 
 
-def test_utility_func():
-    dis_util_work = 0.3
-    dis_util_unemployed = 0.8
-    mu = 0.5
-
+@pytest.mark.parametrize(
+    "consumption, dis_util_work, dis_util_unemployed, mu",
+    list(
+        product(CONSUMPTION_GRID, DISUTIL_WORK_GRID, DISUTIL_UNEMPLOYED_GRID, MU_GRID)
+    ),
+)
+def test_utility_func(consumption, dis_util_work, dis_util_unemployed, mu):
     params = {
         "mu": mu,
         "dis_util_work": dis_util_work,
         "dis_util_unemployed": dis_util_unemployed,
     }
-    cons = 2
-    cons_utility = cons ** (1 - mu) / mu
+    cons_utility = consumption ** (1 - mu) / (1 - mu)
 
     np.testing.assert_almost_equal(
-        utility_func(cons, 1, params), cons_utility - dis_util_work
+        utility_func(consumption=consumption, choice=1, params=params),
+        cons_utility - dis_util_work,
     )
     np.testing.assert_almost_equal(
-        utility_func(cons, 0, params), cons_utility - dis_util_unemployed
+        utility_func(consumption=consumption, choice=0, params=params),
+        cons_utility - dis_util_unemployed,
     )

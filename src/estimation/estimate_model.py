973d2255d437d3e9233274b4e60bf58c65900c64
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-02-19 20:49:31 -0500

    Started with estimagic.

--- src/estimation/estimate_model.py
+++ src/estimation/estimate_model.py
@@ -1,4 +1,4 @@
-import time
+import pickle
 from pathlib import Path
 
 import numpy as np
@@ -8,9 +8,6 @@ analysis_path = str(Path(__file__).resolve().parents[2]) + "/"
 file_dir_path = str(Path(__file__).resolve().parents[0]) + "/"
 import sys
 import yaml
-from jax.flatten_util import ravel_pytree
-import pickle
-import scipy.optimize as opt
 import jax
 
 jax.config.update("jax_enable_x64", True)
@@ -27,7 +24,6 @@ data_decision = data_decision[data_decision["lagged_choice"] != 2]
 # Load data specs
 from derive_specs import generate_derived_and_data_derived_specs
 
-start = time.time()
 project_paths = {
     "project_path": analysis_path,
 }
@@ -71,37 +67,60 @@ params_to_estimate_names = [
     "dis_util_work",
     "dis_util_unemployed",
     "bequest_scale",
-    # "lambda",
+    "lambda",
     # "sigma",
 ]
 start_params = {name: start_params_all[name] for name in params_to_estimate_names}
 
+import estimagic as em
 
-# Create likelihood function for estimation
-params_start_vec, unravel_func = ravel_pytree(start_params)
+lower_bounds = {
+    "dis_util_work": 1e-12,
+    "dis_util_unemployed": 1e-12,
+    "bequest_scale": 1e-12,
+    "lambda": 1e-12,
+}
+upper_bounds = {
+    "dis_util_work": 100,
+    "dis_util_unemployed": 100,
+    "bequest_scale": 10,
+    "lambda": 100,
+}
 
-
-def individual_likelihood_vec(params_vec):
-    params = unravel_func(params_vec)
-    ll_value = individual_likelihood(params)
-    print("Params, ", params, " with ll value, ", ll_value)
-    return ll_value
-
-
-bounds = [
-    (1e-12, 100),
-    (1e-12, 100),
-    (1e-12, 100),
-    # (1e-12, 10)
-]
-
-result = opt.minimize(
-    individual_likelihood_vec,
-    params_start_vec,
-    bounds=bounds,
-    method="L-BFGS-B",
+result = em.minimize(
+    criterion=individual_likelihood,
+    params=start_params,
+    lower_bounds=lower_bounds,
+    upper_bounds=upper_bounds,
+    algorithm="scipy_lbfgsb",
+    logging="test_log.db",
+    error_handling="continue",
 )
 pickle.dump(result, open(file_dir_path + "res.pkl", "wb"))
+
+#
+# # Create likelihood function for estimation
+# params_start_vec, unravel_func = ravel_pytree(start_params)
+#
+# bounds = [
+# (1e-12, 100),
+# (1e-12, 100),
+# (1e-12, 100),
+# (1e-12, 10)
+# ]
+# def individual_likelihood_vec(params_vec):
+#     params = unravel_func(params_vec)
+#     ll_value = individual_likelihood(params)
+#     print("Params, ", params, " with ll value, ", ll_value)
+#     return ll_value
+#
+# result = opt.minimize(
+#     individual_likelihood_vec,
+#     params_start_vec,
+#     bounds=bounds,
+#     method="L-BFGS-B",
+# )
+#
 
 
 # from dcegm.solve import get_solve_func_for_model

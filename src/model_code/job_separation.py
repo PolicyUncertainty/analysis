c2d80380ade4e59e821ddb34b7547a53f95fc029
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-07-17 16:37:41 +0200

    Draft.

---
+++ src/model_code/job_separation.py
@@ -0,0 +1,27 @@
+def job_offer_process_transition(params, options, education, period, choice):
+    """Transition probability for next period job offer state.
+
+    The values of process are the following:
+    - 0: No job offer in case of unemployment and job destruction in case of employment
+    - 1: Job offer in case of unemployment and no job destruction in case of employment
+
+    """
+
+    unemployment_choice = choice == 0
+    labor_choice = choice == 1
+
+    # Probability of job destruction
+    job_sep_prob = options["job_sep_probs"][education, period]
+
+    job_finding_prob = calc_job_finding_prob(params, education, period)
+
+    # Transition probability
+    prob_value_0 = (
+        job_sep_prob * labor_choice + (1 - job_finding_prob) * unemployment_choice
+    )
+
+    return prob_value_0, 1 - prob_value_0
+
+
+def calc_job_finding_prob(params, education, period):
+    pass

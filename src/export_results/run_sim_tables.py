73ade74d7842d454f6004cb7acffc636c25ac7a0
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-09-24 16:59:48 +0200

    New estimates.

--- src/export_results/run_sim_tables.py
+++ src/export_results/run_sim_tables.py
@@ -6,7 +6,6 @@ from set_paths import create_path_dict
 
 path_dict = create_path_dict()
 params = pickle.load(open(path_dict["est_results"] + "est_params_all.pkl", "rb"))
-
 from export_results.tables.effects_table import create_effects_table
 
 sim_dir = path_dict["intermediate_data"] + "sim_data/"
@@ -15,48 +14,21 @@ sim_dir = path_dict["intermediate_data"] + "sim_data/"
 ###############################################
 data_no_unc = pd.read_pickle(sim_dir + "data_real_scale_1.pkl")
 data_unc = pd.read_pickle(sim_dir + "data_subj_scale_1.pkl")
-# create_effects_table(
-#     df_base=data_unc,
-#     df_cf=data_no_unc,
-#     params=params,
-#     path_dict=path_dict,
-#     scenario_name="no_uncertainty",
-# )
+create_effects_table(
+    df_base=data_unc,
+    df_cf=data_no_unc,
+    params=params,
+    path_dict=path_dict,
+    scenario_name="no_uncertainty",
+)
 
 ###############################################
 # Policy trajectories
 ###############################################
-from export_results.tables.cv import create_real_utility, create_disc_sum
-
 data_no_inc = pd.read_pickle(sim_dir + "data_no_increase.pkl")
-data_no_inc = create_real_utility(data_no_inc)
-disc_sum_no_inc = create_disc_sum(data_no_inc, params, reset_index=True)
-
-data_no_inc_no_unc = pd.read_pickle(sim_dir + "data_no_inc_no_unc.pkl")
-data_no_inc_no_unc = create_real_utility(data_no_inc_no_unc)
-disc_sum_no_inc_no_unc = create_disc_sum(data_no_inc_no_unc, params, reset_index=True)
-
 data_005 = pd.read_pickle(sim_dir + "data_incr_005.pkl")
 data_01 = pd.read_pickle(sim_dir + "data_incr_01.pkl")
 
-
-def print_av_ret_age(df):
-    print(
-        df[(df["choice"] == 2) & (df["lagged_choice"] != 2)]["age"].value_counts(
-            normalize=True
-        )
-    )
-    df["policy_state_val"] = df["policy_state"] * 0.25 + 65
-    print(
-        df[(df["choice"] == 2) & (df["lagged_choice"] != 2)][
-            "policy_state_val"
-        ].value_counts(normalize=True)
-    )
-
-
-print_av_ret_age(data_no_inc)
-print_av_ret_age(data_005)
-print_av_ret_age(data_01)
 create_effects_table(
     df_base=data_no_inc,
     df_cf=data_005,
@@ -71,3 +43,20 @@ create_effects_table(
     path_dict=path_dict,
     scenario_name="0_1_inc",
 )
+
+# from export_results.tables.cv import create_real_utility, create_disc_sum
+# def print_av_ret_age(df):
+#     print(
+#         df[(df["choice"] == 2) & (df["lagged_choice"] != 2)]["age"].mean()
+#     )
+#     df["policy_state_val"] = df["policy_state"] * 0.25 + 65
+#     print(
+#         df[(df["choice"] == 2) & (df["lagged_choice"] != 2)][
+#             "policy_state_val"
+#         ].mean()
+#     )
+#
+#
+# print_av_ret_age(data_no_inc)
+# # print_av_ret_age(data_005)
+# # print_av_ret_age(data_01)

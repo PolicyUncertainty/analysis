308a113c22025234cb9fe363c1f7f9602fd36195
brunoVeltri <bruno.veltri@hu-berlin.de>
2024-10-08 19:39:58 +0200

    util funcs

--- src/model_code/utility_functions.py
+++ src/model_code/utility_functions.py
@@ -3,9 +3,9 @@ import jax.numpy as jnp
 
 def create_utility_functions():
     return {
-        "utility": utility_func,
-        "inverse_marginal_utility": inverse_marginal,
-        "marginal_utility": marg_utility,
+        "utility": utility_func_CD,
+        "inverse_marginal_utility": inverse_marginal_CD,
+        "marginal_utility": marg_utility_CD,
     }
 
 
@@ -82,3 +82,53 @@ def consumption_scale(partner_state, education, period, options):
     nb_children = options["children_by_state"][0, education, has_partner, period]
     hh_size = 1 + has_partner + nb_children
     return jnp.sqrt(hh_size)
+
+
+def utility_func_CD(
+    consumption, partner_state, education, period, choice, params, options
+):
+    """Calculate the choice specific cobb-douglas utility, i.e. u = ((c*eta/consumption_scale)^(1-mu))/(1-mu) .""" 
+    # gather params
+    mu = params["mu"]
+    eta = disutility_work(choice, education, params)
+    cons_scale = consumption_scale(partner_state, education, period, options)
+    # compute utility
+    utility = ((consumption * eta / cons_scale) ** (1 - mu) - 1) / (1 - mu)
+    return utility
+
+def disutility_work (choice, education, params):
+    # reading parameters
+    dis_util_work = (
+        params["dis_util_work_low"] * (1 - education)
+        + params["dis_util_work_high"] * education
+    )
+    dis_util_unemployed = (
+        params["dis_util_unemployed_low"] * (1 - education)
+        + params["dis_util_unemployed_high"] * education
+    )
+
+    # dis_util_only_partner_retired = params["dis_util_only_partner_retired"]
+
+    # choice booleans
+    is_working = choice == 1
+    is_unemployed = choice == 0
+    # partner_retired = partner_state == 0
+
+    # compute eta
+    disutility = jnp.exp(-dis_util_work * is_working  - dis_util_unemployed* is_unemployed)
+    # disutility = jnp.exp(-dis_util_work * is_working - dis_util_unemployed * is_unemployed - dis_util_only_partner_retired * partner_retired)
+    return disutility
+
+def marg_utility_CD(consumption, partner_state, education, period, params, options):
+    cons_scale = consumption_scale(partner_state, education, period, options)
+    mu = params["mu"]
+    eta = disutility_work(1, education, params)
+    marg_util = (consumption * eta / cons_scale) ** -mu * (1 / cons_scale)
+    return marg_util
+
+def inverse_marginal_CD(marginal_utility, partner_state, education, period, params, options):
+    cons_scale = consumption_scale(partner_state, education, period, options)
+    mu = params["mu"]
+    eta = disutility_work(1, education, params)
+    consumption = ((marginal_utility * cons_scale) ** (-1 / mu)) * cons_scale / eta
+    return consumption

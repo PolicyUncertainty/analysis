8ac2b1e9fa58d3672b90158b67c84ad6d4cdb73e
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-10-08 16:10:03 +0200

    Need to delete error first.

--- src/export_results/figures/observed_model_fit.py
+++ src/export_results/figures/observed_model_fit.py
@@ -94,6 +94,9 @@ def observed_model_fit(paths_dict):
     neg_likelihood_contributions = (-np.log(prob_choice_observed)).clip(max=999)
     org_ll = np.loadtxt("ll_org.txt")
     org_probs = np.exp(-org_ll)
+    data_decision["prob_choice_org"] = org_probs
+    data_decision = data_decision[data_decision["lagged_choice"] != 2]
+    data_decision = data_decision[data_decision["age"] > 70]
 
     # breakpoint()
 

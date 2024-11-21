878c390976c57dc3fc15525b599941f6c4d583ab
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-11-21 16:10:15 +0100

    Finish weight function.

--- src/model_code/unobserved_state_weighting.py
+++ src/model_code/unobserved_state_weighting.py
@@ -15,6 +15,21 @@ def create_unobserved_state_specs(data_decision, model):
             kwargs["job_offer_observed_bool"], 0.5, job_offer_weight_unobserved
         )
 
+        # For the informed state we use the share of this period. The period in the kwargs is the one from
+        # before (see assignment below).
+        this_period = kwargs["period"] + 1
+        education = kwargs["education"]
+        informed_share = kwargs["options"]["informed_shares"][this_period, education]
+        informed_new = kwargs["informed_new"]
+        unobserved_informed_weight = informed_share * informed_new + (
+            1 - informed_share
+        ) * (1 - informed_new)
+        # Again, if informed is observed we set the weight to 1 / n_informed_states
+        informed_weight = jax.lax.select(
+            kwargs["informed_observed_bool"], 0.5, unobserved_informed_weight
+        )
+        return job_offer_weight * informed_weight
+
     relevant_prev_period_state_choices_dict = {
         "period": data_decision["period"].values - 1,
         "education": data_decision["education"].values,

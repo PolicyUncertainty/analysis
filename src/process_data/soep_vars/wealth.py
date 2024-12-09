a8897d2485ff323f8c2945bf1595d0d4caeaa13b
brunoVeltri <bruno.veltri@hu-berlin.de>
2024-12-09 19:29:21 +0100

    debugged

--- src/process_data/soep_vars/wealth.py
+++ src/process_data/soep_vars/wealth.py
@@ -17,6 +17,24 @@ def add_wealth_interpolate_and_deflate(data, path_dict, options):
     print(str(len(data)) + " left after dropping people with missing wealth.")
     return data
 
+def add_wealth_impute_with_panel_reg(data, path_dict, options):
+    """Loads wealth data, imputes missing values using panel regression.
+    Note: we do not de-/ inflate the wealth variable but estimate a linear time trend and predict using a reference year.
+    """
+    data = data.reset_index()
+    wealth_data = load_wealth_data(path_dict["soep_c38"])
+    wealth_data = trim_and_rename(wealth_data)
+    wealth_data_with_covariates, cov_list = add_hh_wealth_covariates(wealth_data, path_dict["soep_c38"])
+    fitted_model = estimate_imputation_params_with_panel_ols(wealth_data_with_covariates, cov_list, options)
+    wealth_data_full = span_full_wealth_panel(wealth_data_with_covariates, options)
+    wealth_data_full_with_covariates, cov_list = add_hh_wealth_covariates(wealth_data_full, path_dict["soep_c38"])
+    wealth_data_full_imputed = impute_wealth_with_panel_reg(wealth_data_full_with_covariates, fitted_model, cov_list, options)
+
+    data = data.merge(wealth_data_full_imputed, on=["hid", "syear"], how="left")
+    data.set_index(["pid", "syear"], inplace=True)
+    print(str(len(data)) + " left after dropping people with missing wealth.")
+    return data
+
 
 def load_wealth_data(soep_c38_path):
     # Load SOEP core data
@@ -68,41 +86,28 @@ def deflate_wealth(df, path_dict):
     return df
 
 
-def add_wealth_impute_with_panel_reg(data, path_dict, options):
-    """Loads wealth data, imputes missing values using panel regression, and deflates wealth using the consumer price index."""
-    data = data.reset_index()
-    wealth_data = load_wealth_data(path_dict["soep_c38"])
-    wealth_data = trim_and_rename(wealth_data)
-    wealth_data_with_covariates, cov_list = add_hh_wealth_covariates(wealth_data, path_dict["soep_c38"])
-    fitted_model = estimate_imputation_params_with_panel_ols(wealth_data_with_covariates, cov_list)
-    wealth_data_full = span_full_wealth_panel(wealth_data_with_covariates, options)
-    wealth_data_full_with_covariates, cov_list = add_hh_wealth_covariates(wealth_data_full, path_dict["soep_c38"])
-    wealth_data_full_imputed = impute_wealth_with_panel_reg(wealth_data_full_with_covariates, fitted_model, cov_list)
-    data = data.merge(wealth_data_full_imputed, on=["hid", "syear"], how="left")
-    data.set_index(["pid", "syear"], inplace=True)
-    print(str(len(data)) + " left after dropping people with missing wealth.")
-    return data
-
 def add_hh_wealth_covariates(df, soep_path):
     """This function gets the following household level covariates from the hl data:
-    - household net incomme: hlc0005_h
-    - number of children in household: hlc0043
+    - household net income: hlc0005_h
     """
-    covariate_list = ["hlc0005_h", "hlc0043"]
+    covariate_list = ["hlc0005_h"]
+    columns = ["hid", "syear"] + covariate_list
     hl_data = pd.read_stata(
         f"{soep_path}/hl.dta",
-        columns=["hid", "syear", "hlc0005_h", "hlc0043"],
+        columns=columns,
         convert_categoricals=False,
     )
     hl_data.set_index(["hid", "syear"], inplace=True)
-    df.merge(hl_data, on=["hid", "syear"], how="left")
-    breakpoint()
-    df = df[(df["hlc0005_h"]>0) & (df["hlc0043"]>0)]
-    breakpoint()
+    df = df.drop(columns=covariate_list, errors="ignore")
+    df = df.merge(hl_data, on=["hid", "syear"], how="left")
+    df = df[df["hlc0005_h"]>0]
     return df, covariate_list
 
-def estimate_imputation_params_with_panel_ols(data, cov_list):
-    """Estimates the parameters of the panel regression for imputing wealth."""
+def estimate_imputation_params_with_panel_ols(data, cov_list, options):
+    """Estimates the parameters of the panel regression for imputing wealth with a linear time trend."""
+    data.set_index(["hid", "syear"], inplace=True)
+    data["year"] = data.index.get_level_values("syear") - options["start_year"]
+    cov_list = cov_list + ["year"]
     model = PanelOLS(
         dependent=data["wealth"],
         exog=data[cov_list],
@@ -127,9 +132,20 @@ def span_full_wealth_panel(wealth_data, options):
     wealth_data_full.set_index(["hid", "syear"], inplace=True)
     return wealth_data_full
 
-def impute_wealth_with_panel_reg(wealth_data_with_covariates, fitted_model, cov_list):
-    """Imputes missing values of wealth using the panel regression."""
-    wealth_data_with_covariates["wealth"] = fitted_model.predict(
-        wealth_data_with_covariates[cov_list]
+def impute_wealth_with_panel_reg(wealth_data_with_covariates, fitted_model, cov_list, options):
+    """Imputes missing values of wealth using the panel regression, de-/ or inflating the wealth variable by setting the year to the reference year."""
+    # debugging
+    entity_effects = fitted_model.estimated_effects.unstack(level=1).mean(axis=1)
+
+    # set year to reference year
+    wealth_data_with_covariates["year"] = options["reference_year"] - options["start_year"]
+    exog = cov_list + ["year"]
+    # predict wealth
+    wealth_data_with_covariates["wealth_predicted"] = fitted_model.predict(
+        wealth_data_with_covariates[exog]
     )
+    wealth_data_with_covariates["wealth"] = wealth_data_with_covariates["wealth"].combine_first(
+        wealth_data_with_covariates["wealth_predicted"]
+    )
+    breakpoint()
     return wealth_data_with_covariates

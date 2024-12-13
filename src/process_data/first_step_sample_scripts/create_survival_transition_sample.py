76a7c9ff9acda857fe250a674965fe4e3eb94538
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-12-13 15:58:20 +0100

    Slight change.

--- src/process_data/first_step_sample_scripts/create_survival_transition_sample.py
+++ src/process_data/first_step_sample_scripts/create_survival_transition_sample.py
@@ -1,7 +1,8 @@
 # %%
 import os
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 from process_data.aux_scripts.filter_data import filter_above_age
 from process_data.aux_scripts.filter_data import filter_below_age
 from process_data.aux_scripts.filter_data import filter_by_sex
@@ -30,57 +31,80 @@ def create_survival_transition_sample(paths, specs, load_data=False):
     # filter age and estimation years
     df = filter_below_age(df, 16)
     df = filter_above_age(df, 110)
-    df = filter_years(df, specs["start_year_mortality"], specs["end_year_mortality"])
- 
+    # df = filter_years(df, specs["start_year_mortality"], specs["end_year_mortality"])
 
     # Fill health states
     # IF: The current value of health is missing (mi(health[_n])).
     # The value x observations ahead (health[_n+x']) is not missing (!mi(health[_n+x'])). AND
     # The value immediately before the current observation (health[_n-1]) is not missing (!mi(health[_n-1])). AND
-    # The values immediately before the current observation (health[_n-1]) and x observations ahead (health[_n+x']) are equal (health[_n-1] == health[_n+x']). 
+    # The values immediately before the current observation (health[_n-1]) and x observations ahead (health[_n+x']) are equal (health[_n-1] == health[_n+x']).
     # THEN: the missing value of health at the current position (_n) is replaced by the value of health[_n+x']`.
-
 
     # fill gaps were first and last known health state are identical with that value
     def fill_health_gaps(group):
         # Forward-fill and backward-fill
-        ffilled = group['health_state'].ffill()
-        bfilled = group['health_state'].bfill()
+        ffilled = group["health_state"].ffill()
+        bfilled = group["health_state"].bfill()
         # Create a mask where forward-fill and backward-fill agree
         agreeing_mask = ffilled == bfilled
         # Fill only where the mask is True
-        group['health_state'] = group['health_state'].where(~group['health_state'].isna() | ~agreeing_mask, ffilled)
+        group["health_state"] = group["health_state"].where(
+            ~group["health_state"].isna() | ~agreeing_mask, ffilled
+        )
         return group
 
-    # Fill health gaps 
+    # Fill health gaps
     df = df.sort_index()
-    df = df.groupby("pid").apply(fill_health_gaps) 
-    df.index = df.index.droplevel(1) # remove extra pid index level -> index = (pid, syear) instead of (pid, pid, syear)
+    df = df.groupby("pid").apply(fill_health_gaps)
+    df.index = df.index.droplevel(
+        1
+    )  # remove extra pid index level -> index = (pid, syear) instead of (pid, pid, syear)
 
     print("Obs. after filling health gaps:", len(df))
 
     # print number of death events for the entire sample with and without missing health data
     print("Death events in the sample: ", df["event_death"].sum())
-    print("Death events in the sample without missing health data: ", df[df["health_state"].notna()]["event_death"].sum())
-    print("Number of observations in the sample with health 1 in the death year:", len(df[(df["event_death"] == 1) & (df["health_state"] == 1)]))
-    print("Number of observations in the sample with health 0 in the death year:", len(df[(df["event_death"] == 1) & (df["health_state"] == 0)]))
+    print(
+        "Death events in the sample without missing health data: ",
+        df[df["health_state"].notna()]["event_death"].sum(),
+    )
+    print(
+        "Number of observations in the sample with health 1 in the death year:",
+        len(df[(df["event_death"] == 1) & (df["health_state"] == 1)]),
+    )
+    print(
+        "Number of observations in the sample with health 0 in the death year:",
+        len(df[(df["event_death"] == 1) & (df["health_state"] == 0)]),
+    )
 
-    
     # for deaths, set the health state to the last known health state
     df["last_known_health_state"] = df.groupby("pid")["health_state"].transform("last")
-    df.loc[(df["event_death"] == 1) & (df["health_state"].isna()), "health_state"] = df.loc[(df["event_death"] == 1) & (df["health_state"].isna()), "last_known_health_state"]
+    df.loc[
+        (df["event_death"] == 1) & (df["health_state"].isna()), "health_state"
+    ] = df.loc[
+        (df["event_death"] == 1) & (df["health_state"].isna()),
+        "last_known_health_state",
+    ]
     print("Setting health state to last known health state for deaths.")
 
     # print number of death events for the entire sample with and without missing health data
     print("Death events in the sample: ", df["event_death"].sum())
-    print("Death events in the sample without missing health data: ", df[df["health_state"].notna()]["event_death"].sum())
-    print("Number of observations in the sample with health 1 in the death year:", len(df[(df["event_death"] == 1) & (df["health_state"] == 1)]))
-    print("Number of observations in the sample with health 0 in the death year:", len(df[(df["event_death"] == 1) & (df["health_state"] == 0)]))
+    print(
+        "Death events in the sample without missing health data: ",
+        df[df["health_state"].notna()]["event_death"].sum(),
+    )
+    print(
+        "Number of observations in the sample with health 1 in the death year:",
+        len(df[(df["event_death"] == 1) & (df["health_state"] == 1)]),
+    )
+    print(
+        "Number of observations in the sample with health 0 in the death year:",
+        len(df[(df["event_death"] == 1) & (df["health_state"] == 0)]),
+    )
 
     # drop if the health state is missing
     df = df[(df["health_state"].notna())]
     print("Obs. after dropping missing health data:", len(df))
-
 
     # for every pid find first year in the sample observations and set the begin age and health state
     df = df.reset_index()
@@ -91,10 +115,19 @@ def create_survival_transition_sample(paths, specs, load_data=False):
     df["begin_health_state"] = df.groupby("pid")["begin_health_state"].transform("max")
     df = df.set_index(["pid", "syear"])
 
+    df = df[
+        [
+            "age",
+            "begin_age",
+            "event_death",
+            "education",
+            "sex",
+            "health_state",
+            "begin_health_state",
+        ]
+    ]
 
-    df = df[["age", "begin_age", "event_death", "education", "sex", "health_state", "begin_health_state"]]
-
-    # set the dtype of the columns to float 
+    # set the dtype of the columns to float
     df = df.astype(float)
 
     # Show data overview
@@ -111,13 +144,24 @@ def create_survival_transition_sample(paths, specs, load_data=False):
     print("Average age in the sample:", round(df["age"].mean(), 2))
 
     # print the number of unique individuals in the sample
-    print("Number of unique individuals in the sample:", df.index.get_level_values("pid").nunique())
+    print(
+        "Number of unique individuals in the sample:",
+        df.index.get_level_values("pid").nunique(),
+    )
 
     # print the number of unique years in the sample (min and max)
-    print("Sample Years:", df.index.get_level_values("syear").min(), "-", df.index.get_level_values("syear").max())
+    print(
+        "Sample Years:",
+        df.index.get_level_values("syear").min(),
+        "-",
+        df.index.get_level_values("syear").max(),
+    )
 
     # Average time spent in the sample for each individual
-    print("Average time spent in the sample for each individual:", round(df.groupby("pid").size().mean(), 2))
+    print(
+        "Average time spent in the sample for each individual:",
+        round(df.groupby("pid").size().mean(), 2),
+    )
 
     print(
         str(len(df))
@@ -135,7 +179,7 @@ def load_and_merge_datasets(soep_c38_path, specs):
     df = pd.merge(annual_survey_data, life_spell_data, on=["pid", "syear"], how="inner")
     df = pd.merge(df, health_data, on=["pid", "syear"], how="inner")
     df = df.set_index(["pid", "syear"])
-    return df 
+    return df
 
 
 def load_and_process_soep_yearly_survey_data(soep_c38_path, specs):
@@ -160,7 +204,9 @@ def load_and_process_soep_yearly_survey_data(soep_c38_path, specs):
     merged_data.set_index(["pid", "syear"], inplace=True)
 
     # Pre-Filter estimation years
-    df = filter_years(merged_data, specs["start_year_mortality"], specs["end_year_mortality"])
+    df = filter_years(
+        merged_data, specs["start_year_mortality"], specs["end_year_mortality"]
+    )
     df = filter_by_sex(df, no_women=False)
     # Create education type
     df = create_education_type(df)
@@ -243,21 +289,23 @@ def load_and_process_life_spell_data(soep_c38_path, specs):
 
 
 def load_and_process_soep_health(soep_c38_path, specs):
-
     pequiv_data = pd.read_stata(
-    # m11126: Self-Rated Health Status
-    # m11124: Disability Status of Individual
-    f"{soep_c38_path}/pequiv.dta",
-    columns=["pid", "syear", "m11126", "m11124"],
-    convert_categoricals=False,
+        # m11126: Self-Rated Health Status
+        # m11124: Disability Status of Individual
+        f"{soep_c38_path}/pequiv.dta",
+        columns=["pid", "syear", "m11126", "m11124"],
+        convert_categoricals=False,
     )
     pequiv_data.set_index(["pid", "syear"], inplace=True)
 
     # create health states
-    pequiv_data = filter_years(pequiv_data, specs["start_year_mortality"] - 1, specs["end_year_mortality"] + 1)
+    pequiv_data = filter_years(
+        pequiv_data, specs["start_year_mortality"] - 1, specs["end_year_mortality"] + 1
+    )
     pequiv_data = create_health_var(pequiv_data)
-    pequiv_data = span_dataframe(pequiv_data, specs["start_year_mortality"] - 1, specs["end_year_mortality"] + 1)
+    pequiv_data = span_dataframe(
+        pequiv_data, specs["start_year_mortality"] - 1, specs["end_year_mortality"] + 1
+    )
     pequiv_data = clean_health_create_states(pequiv_data)
 
     return pequiv_data
-

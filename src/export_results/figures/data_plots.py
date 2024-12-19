9904f56ac7f6ede84e6efe5b36bb1c8902af6911
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-12-19 12:53:24 +0100

    Incorporated mean and median options for plotting.

--- src/export_results/figures/data_plots.py
+++ src/export_results/figures/data_plots.py
@@ -28,7 +28,9 @@ def plot_data_choices(path_dict, lagged=False):
         if not lagged:
             choice_shares = edu_df.groupby("age")["choice"].value_counts(normalize=True)
         else:
-            choice_shares = edu_df.groupby("age")["lagged_choice"].value_counts(normalize=True)
+            choice_shares = edu_df.groupby("age")["lagged_choice"].value_counts(
+                normalize=True
+            )
         # Initialize zero shares for all ages and choices
         full_shares = pd.Series(index=multi_index, data=0)
         full_shares[choice_shares.index] = choice_shares.values
@@ -39,10 +41,11 @@ def plot_data_choices(path_dict, lagged=False):
             axs[edu, choice].set_title(f"{edu_labels[edu]}: {choice_labels[choice]}")
             axs[edu, choice].set_ylim(-0.05, 1.05)
 
+
 def plot_state_by_age_and_type(path_dict, state_vars):
     struct_est_sample = pd.read_pickle(path_dict["struct_est_sample"])
     specs = generate_derived_and_data_derived_specs(path_dict, load_precomputed=True)
-    
+
     # age restriction
     struct_est_sample["age"] = struct_est_sample["period"] + specs["start_age"]
     start = specs["start_age"]
@@ -52,20 +55,38 @@ def plot_state_by_age_and_type(path_dict, state_vars):
     plot_ages = np.arange(start, end + 1)
 
     n_education_types = specs["n_education_types"]
-    n_state_vars = len(state_vars) 
-    fig, axs = plt.subplots(nrows = n_education_types, ncols = n_state_vars, figsize=(15, 5* n_education_types))
+    n_state_vars = len(state_vars)
+    fig, axs = plt.subplots(
+        nrows=n_education_types, ncols=n_state_vars, figsize=(15, 5 * n_education_types)
+    )
 
     for edu in range(n_education_types):
         edu_df = struct_est_sample[struct_est_sample["education"] == edu]
         for idx, state_var in enumerate(state_vars):
-            state_values = edu_df.groupby("age")[state_var].mean().reindex(plot_ages, fill_value=np.nan).values
+            if "median" in state_var:
+                col_name = state_var.split(" ")[1]
+                state_values = (
+                    edu_df.groupby("age")[col_name]
+                    .median()
+                    .reindex(plot_ages, fill_value=np.nan)
+                    .values
+                )
+            elif "mean" in state_var:
+                col_name = state_var.split(" ")[1]
+                state_values = (
+                    edu_df.groupby("age")[col_name]
+                    .mean()
+                    .reindex(plot_ages, fill_value=np.nan)
+                    .values
+                )
+            else:
+                raise ValueError("Only median and mean are supported")
             ax = axs[edu, idx] if n_education_types > 1 else axs[idx]
             ax.plot(plot_ages, state_values, label=state_var)
             ax.legend()
             ax.set_title(f"{specs['education_labels'][edu]}: {state_var}")
-            ax.set_xlabel('Age')
+            ax.set_xlabel("Age")
             ax.set_ylabel(state_var)
             ax.set_ylim(bottom=0)  # Adjust as needed
 
     plt.tight_layout()
-        
--- src/export_results/run_data_plots.py
+++ src/export_results/run_data_plots.py
@@ -1,5 +1,4 @@
 # This script is used to run the data_plots.py script to generate plots of the structural estimation dataset.
-
 # %%
 import matplotlib.pyplot as plt
 from set_paths import create_path_dict
@@ -8,6 +7,7 @@ path_dict = create_path_dict()
 
 # %%
 from export_results.figures.data_plots import plot_data_choices
+
 show_choices = input("Show data choice plots? (y/n) ") == "y"
 if show_choices:
     plot_data_choices(path_dict)
@@ -20,9 +20,15 @@ if show_lagged_choices:
     plt.close("all")
 # %%
 from export_results.figures.data_plots import plot_state_by_age_and_type
+
 show_state = input("Show state plots? (y/n) ") == "y"
 if show_state:
-    state_vars = ["experience", "wealth", "health_state", "experience"]
+    state_vars = [
+        "mean experience",
+        "mean wealth",
+        "mean health_state",
+        "median wealth",
+    ]
     plot_state_by_age_and_type(path_dict, state_vars=state_vars)
     plt.show()
-    plt.close("all")    
+    plt.close("all")

37b9dfb28929022ccd662bd2557ed2aec53b5a3b
Maximilian Blesch <maximilian.blesch@hu-berlin.de>
2024-12-19 12:55:50 +0100

    Implement new wealth interpolation (#55)
    Co-authored-by: brunoVeltri <bruno.veltri@hu-berlin.de>

--- src/export_results/figures/data_plots.py
+++ src/export_results/figures/data_plots.py
@@ -4,11 +4,10 @@ import pandas as pd
 from specs.derive_specs import generate_derived_and_data_derived_specs
 
 
-def plot_data_choices(path_dict):
+def plot_data_choices(path_dict, lagged=False):
     struct_est_sample = pd.read_pickle(path_dict["struct_est_sample"])
 
     specs = generate_derived_and_data_derived_specs(path_dict, load_precomputed=True)
-
     struct_est_sample["age"] = struct_est_sample["period"] + specs["start_age"]
     struct_est_sample = struct_est_sample[struct_est_sample["age"] < 75]
 
@@ -26,8 +25,12 @@ def plot_data_choices(path_dict):
 
     for edu in range(specs["n_education_types"]):
         edu_df = struct_est_sample[struct_est_sample["education"] == edu]
-        choice_shares = edu_df.groupby("age")["choice"].value_counts(normalize=True)
-
+        if not lagged:
+            choice_shares = edu_df.groupby("age")["choice"].value_counts(normalize=True)
+        else:
+            choice_shares = edu_df.groupby("age")["lagged_choice"].value_counts(
+                normalize=True
+            )
         # Initialize zero shares for all ages and choices
         full_shares = pd.Series(index=multi_index, data=0)
         full_shares[choice_shares.index] = choice_shares.values
@@ -37,3 +40,53 @@ def plot_data_choices(path_dict):
             axs[edu, choice].plot(plot_ages, choice_shares)
             axs[edu, choice].set_title(f"{edu_labels[edu]}: {choice_labels[choice]}")
             axs[edu, choice].set_ylim(-0.05, 1.05)
+
+
+def plot_state_by_age_and_type(path_dict, state_vars):
+    struct_est_sample = pd.read_pickle(path_dict["struct_est_sample"])
+    specs = generate_derived_and_data_derived_specs(path_dict, load_precomputed=True)
+
+    # age restriction
+    struct_est_sample["age"] = struct_est_sample["period"] + specs["start_age"]
+    start = specs["start_age"]
+    end = specs["max_ret_age"]
+
+    struct_est_sample = struct_est_sample[struct_est_sample["age"] < end]
+    plot_ages = np.arange(start, end + 1)
+
+    n_education_types = specs["n_education_types"]
+    n_state_vars = len(state_vars)
+    fig, axs = plt.subplots(
+        nrows=n_education_types, ncols=n_state_vars, figsize=(15, 5 * n_education_types)
+    )
+
+    for edu in range(n_education_types):
+        edu_df = struct_est_sample[struct_est_sample["education"] == edu]
+        for idx, state_var in enumerate(state_vars):
+            if "median" in state_var:
+                col_name = state_var.split(" ")[1]
+                state_values = (
+                    edu_df.groupby("age")[col_name]
+                    .median()
+                    .reindex(plot_ages, fill_value=np.nan)
+                    .values
+                )
+            elif "mean" in state_var:
+                col_name = state_var.split(" ")[1]
+                state_values = (
+                    edu_df.groupby("age")[col_name]
+                    .mean()
+                    .reindex(plot_ages, fill_value=np.nan)
+                    .values
+                )
+            else:
+                raise ValueError("Only median and mean are supported")
+            ax = axs[edu, idx] if n_education_types > 1 else axs[idx]
+            ax.plot(plot_ages, state_values, label=state_var)
+            ax.legend()
+            ax.set_title(f"{specs['education_labels'][edu]}: {state_var}")
+            ax.set_xlabel("Age")
+            ax.set_ylabel(state_var)
+            ax.set_ylim(bottom=0)  # Adjust as needed
+
+    plt.tight_layout()

b46afc5086c586934a5205edd3c6f8ae5f7486f6
gregor-schueler <142438004+gregor-schueler@users.noreply.github.com>
2025-01-10 15:47:07 +0100

    Refactor Survival Probability Estimation (#59)
    Co-authored-by: MaxBlesch <maximilian.blesch@hu-berlin.de>

--- src/export_results/run_analysis_plots.py
+++ src/export_results/run_analysis_plots.py
@@ -43,7 +43,7 @@ if exec_health:
 ######################################
 exec_mortality = input("Execute Mortality characteristics? (y/n) ") == "y"
 if exec_mortality:
-    from export_results.figures.expected_mortality import (
+    from export_results.figures.plot_mortality import (
         plot_mortality,
     )
 

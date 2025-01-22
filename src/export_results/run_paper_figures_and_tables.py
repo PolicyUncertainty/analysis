e136033623eccb578629c2b6a089e81376f616f0
brunoVeltri <bruno.veltri@hu-berlin.de>
2025-01-22 19:05:30 +0100

    upload figures

--- src/export_results/run_paper_figures_and_tables.py
+++ src/export_results/run_paper_figures_and_tables.py
@@ -1,4 +1,3 @@
-import pickle
 import matplotlib.pyplot as plt
 from set_paths import create_path_dict
 from specs.derive_specs import read_and_derive_specs
@@ -8,9 +7,14 @@ specs = read_and_derive_specs(paths_dict["specs"])
 
 # Ch 2 Inst Background and Data -------------------------------------------------------------------
 
+# chart: pension contribution and payout rates
+from export_results.figures.pension_rates import plot_pension_rates
+plot_pension_rates(paths_dict)
+plt.savefig(paths_dict["paper_plots"] + "pension_rates.png", transparent=True, dpi=300)
+#plt.show()
+
 # chart: 2007 reform
 from export_results.figures.policy_states import plot_SRA_2007_reform
-
 plot_SRA_2007_reform(paths_dict)
 plt.savefig(paths_dict["paper_plots"] + "SRA_2007_reform.png", transparent=True, dpi=300)
 #plt.show()

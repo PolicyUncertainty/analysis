c58352b68725d3453d7f0cdc072360f972560f7c
MaxBlesch <maximilian.blesch@hu-berlin.de>
2025-01-23 00:46:02 +0100

    cont.

--- src/estimation/struct_estimation/run_model_est.py
+++ src/estimation/struct_estimation/run_model_est.py
@@ -52,34 +52,34 @@ params_to_estimate_names = [
 # last_estimate.pop("disutil_unemployed_men")
 # last_estimate.pop("disutil_unemployed_women")
 
-# last_estimate = {
-#     "disutil_pt_work_good_women": 1.1613709241290286,
-#     "disutil_ft_work_good_women": 1.420552617943543,
-#     "disutil_ft_work_bad_women": 1.6456309911389186,
-#     "disutil_pt_work_bad_women": 1.46455074176406,
-#     "disutil_unemployed_women": 0.9001132529936234,
-#     "disutil_children_ft_work_low": 0.1474776294937899,
-#     "disutil_children_ft_work_high": 0.11002761203750037,
-#     "lambda": 0.38401162033036007,
-#     "job_finding_logit_const_women": 0.6743770385955012,
-#     "job_finding_logit_age_women": -0.057388399968934925,
-#     "job_finding_logit_high_educ_women": 0.6849652095064536,
-#     "disutil_unemployed_men": 1.4660151006261883,
-#     "disutil_ft_work_good_men": 0.6255058659669396,
-#     "disutil_ft_work_bad_men": 1.315274144191485,
-#     "job_finding_logit_const_men": 0.7212956269894059,
-#     "job_finding_logit_age_men": -0.03850897252072163,
-#     "job_finding_logit_high_educ_men": 0.1668213335038858,
-# }
+last_estimate = {
+    "lambda": 0.4944967815263844,
+    "disutil_unemployed_men": 1.3828464645020635,
+    "disutil_ft_work_good_men": 0.41631902366821383,
+    "disutil_ft_work_bad_men": 1.2834272651304854,
+    "job_finding_logit_const_men": 0.6889208090678306,
+    "job_finding_logit_age_men": -0.04158755153234817,
+    "job_finding_logit_high_educ_men": -0.1846405005412273,
+    "disutil_unemployed_women": 0.9289395092867999,
+    "disutil_ft_work_good_women": 1.4385501582869862,
+    "disutil_ft_work_bad_women": 1.8218907759136247,
+    "disutil_children_ft_work_low": 0.19135671006901858,
+    "disutil_children_ft_work_high": 0.12283091684623981,
+    "disutil_pt_work_good_women": 1.251966881444964,
+    "disutil_pt_work_bad_women": 1.6824745473129654,
+    "job_finding_logit_const_women": 0.7092057402172162,
+    "job_finding_logit_age_women": -0.058648617500717336,
+    "job_finding_logit_high_educ_women": 0.5967457796021962,
+}
 
 estimation_results = estimate_model(
     paths_dict,
     params_to_estimate_names=params_to_estimate_names,
     file_append="both",
     slope_disutil_method=False,
-    load_model=False,
+    load_model=True,
     use_weights=True,
-    last_estimate=None,
+    last_estimate=last_estimate,
     save_results=False,
 )
 print(estimation_results)

FIx in calculation:

src/simulation/run_counterfactual_2.py
@@ -47,7 +47,6 @@ sra_at_63 = np.arange(67, 70 + specs["SRA_grid_size"], specs["SRA_grid_size"])
 # Create result df
 result_df = pd.DataFrame(
     columns=[
-        "alpha",
         "below_sixty_savings",
         "sra_at_ret",
         "ret_age",
@@ -56,10 +55,10 @@ result_df = pd.DataFrame(
     ],
     dtype=float,
 )
-# Assign alphas in dataframe
+# Assign sras
 result_df["sra_at_63"] = sra_at_63
 for i, sra in enumerate(sra_at_63):
-    print("Start simulation for alpha: ", sra)
+    print("Start simulation for sra: ", sra)
     alpha_sim = (sra - 67) / (63 - specs["start_age"])
 
     # Create estimated model
@@ -85,27 +84,27 @@ for i, sra in enumerate(sra_at_63):
         df_base = df.reset_index().copy()
 
     else:
-        for k, df_scneario in enumerate([df, df_base]):
+        for k, df_scen in enumerate([df, df_base]):
             if k == 0:
                 col_pre = ""
             else:
                 col_pre = "base_"
 
             result_df.loc[i, col_pre + "below_sixty_savings"] = below_sixty_savings(
-                df_scneario
+                df_scen
             )
-            result_df.loc[i, col_pre + "ret_age"] = calc_average_retirement_age(
-                df_scneario
-            )
-            result_df.loc[i, col_pre + "sra_at_ret"] = sra_at_retirement(df_scneario)
-            result_df.loc[i, col_pre + "working_hours"] = df["working_hours"].mean()
+            result_df.loc[i, col_pre + "ret_age"] = calc_average_retirement_age(df_scen)
+            result_df.loc[i, col_pre + "sra_at_ret"] = sra_at_retirement(df_scen)
+            result_df.loc[i, col_pre + "working_hours"] = df_scen[
+                "working_hours"
+            ].mean()
 
-            result_df.loc[i, "cv"] = calc_compensated_variation(
-                df_base=df_base,
-                df_cf=df.reset_index(),
-                params=params,
-                specs=specs,
-            )
+        result_df.loc[i, "cv"] = calc_compensated_variation(
+            df_base=df_base,
+            df_cf=df.reset_index(),
+            params=params,
+            specs=specs,
+        )
 
 # Save results
 result_df.to_csv(path_dict["sim_results"] + f"counterfactual_2_{model_name}.csv")
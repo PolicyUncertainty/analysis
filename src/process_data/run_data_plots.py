019528fe97218a895eafb778d5817dbfe3557171
MaxBlesch <maximilian.blesch@hu-berlin.de>
2025-02-03 13:23:44 +0100

    Added data plots.

--- src/process_data/run_data_plots.py
+++ src/process_data/run_data_plots.py
@@ -8,34 +8,33 @@ from specs.derive_specs import generate_derived_and_data_derived_specs
 
 specs = generate_derived_and_data_derived_specs(path_dict, load_precomputed=True)
 
+which_plots = input(
+    "Which plots do you want to show?\n \n"
+    " - [a]ll\n"
+    " - [r]etirement timing\n"
+    " - [c]hoices\n"
+    " - [s]tates\n"
+    " - [w]ealth\n"
+)
+
 # %% ########################################
 # Retirement timing relative to SRA
-show_retirement_timing = input("Show retirement timing plots? (y/n)") == "y"
-from export_results.figures.retirement_timing import plot_retirement_timing_data
+from process_data.aux_and_plots.retirement_timing import plot_retirement_timing_data
 
-if show_retirement_timing:
+if which_plots in ["a", "r"]:
     plot_retirement_timing_data(path_dict, specs)
-    plt.show()
 
 
 # %%
-from export_results.figures.data_plots import plot_data_choices
+from process_data.aux_and_plots.data_plots import plot_data_choices
 
-show_choices = input("Show data choice plots? (y/n) ") == "y"
-if show_choices:
+if which_plots in ["a", "c"]:
     plot_data_choices(path_dict)
-    plt.show()
-    plt.close("all")
-show_lagged_choices = input("Show lagged data choice plots? (y/n) ") == "y"
-if show_lagged_choices:
     plot_data_choices(path_dict, lagged=True)
-    plt.show()
-    plt.close("all")
 # %%
-from export_results.figures.data_plots import plot_state_by_age_and_type
+from process_data.aux_and_plots.data_plots import plot_state_by_age_and_type
 
-show_state = input("Show state plots? (y/n) ") == "y"
-if show_state:
+if which_plots in ["a", "s"]:
     state_vars = [
         "mean experience",
         "mean wealth",
@@ -43,5 +42,11 @@ if show_state:
         "median wealth",
     ]
     plot_state_by_age_and_type(path_dict, state_vars=state_vars)
-    plt.show()
-    plt.close("all")
+
+from process_data.aux_and_plots.wealth import plot_average_wealth_by_type
+
+if which_plots in ["a", "w"]:
+    plot_average_wealth_by_type(path_dict)
+
+plt.show()
+plt.close("all")
--- src/simulation/run_model_fit_sim.py
+++ src/simulation/run_model_fit_sim.py
@@ -12,8 +12,8 @@ specs = generate_derived_and_data_derived_specs(path_dict)
 model_name = "new"
 load_df = False
 load_solution = True
-load_sim_model = True
-load_sol_model = True
+load_sim_model = False
+load_sol_model = False
 
 
 params = pickle.load(

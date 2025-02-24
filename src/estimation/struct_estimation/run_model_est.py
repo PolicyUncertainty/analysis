268ca5b34495b7d9f02b5cfe59eedb1a879aa8eb
MaxBlesch <maximilian.blesch@hu-berlin.de>
2025-02-24 12:32:13 +0100

    Lets go.

--- src/estimation/struct_estimation/run_model_est.py
+++ src/estimation/struct_estimation/run_model_est.py
@@ -15,33 +15,33 @@ params_to_estimate_names = [
     "disutil_ft_work_low_good_men",
     "disutil_ft_work_low_bad_men",
     # # Women Full-time - 4 parameters
-    # "disutil_ft_work_high_good_women",
-    # "disutil_ft_work_high_bad_women",
-    # "disutil_ft_work_low_good_women",
-    # "disutil_ft_work_low_bad_women",
+    "disutil_ft_work_high_good_women",
+    "disutil_ft_work_high_bad_women",
+    "disutil_ft_work_low_good_women",
+    "disutil_ft_work_low_bad_women",
     # # Women Part-time - 4 parameters
-    # "disutil_pt_work_high_good_women",
-    # "disutil_pt_work_high_bad_women",
-    # "disutil_pt_work_low_good_women",
-    # "disutil_pt_work_low_bad_women",
+    "disutil_pt_work_high_good_women",
+    "disutil_pt_work_high_bad_women",
+    "disutil_pt_work_low_good_women",
+    "disutil_pt_work_low_bad_women",
     # Men unemployment - 2 parameters
     "disutil_unemployed_high_men",
     "disutil_unemployed_low_men",
     # Women Unemployment - 2 parameters
-    # "disutil_unemployed_high_women",
-    # "disutil_unemployed_low_women",
+    "disutil_unemployed_high_women",
+    "disutil_unemployed_low_women",
     # Children - 2 parameters
-    # "disutil_children_ft_work_low",
-    # "disutil_children_ft_work_high",
-    # "lambda",
+    "disutil_children_ft_work_low",
+    "disutil_children_ft_work_high",
+    "lambda",
     # Men job finding - 3 parameters
     "job_finding_logit_const_men",
     "job_finding_logit_age_men",
     "job_finding_logit_high_educ_men",
     # Women job finding - 3 parameters
-    # "job_finding_logit_const_women",
-    # "job_finding_logit_age_women",
-    # "job_finding_logit_high_educ_women",
+    "job_finding_logit_const_women",
+    "job_finding_logit_age_women",
+    "job_finding_logit_high_educ_women",
 ]
 model_name = "partner_est"
 LOAD_LAST_ESTIMATE = True

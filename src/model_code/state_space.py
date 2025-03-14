9e2f4f9def78f24b06aae5ac0a75297532c2c134
brunoVeltri <bruno.veltri@hu-berlin.de>
2025-03-14 15:25:23 +0100

    dummy version implemented in experience transition

--- src/model_code/state_space.py
+++ src/model_code/state_space.py
@@ -155,11 +155,15 @@ def state_specific_choice_set(
 
 
 def apply_retirement_constraint_for_SRA(SRA, options):
+    """ Everyone can retire 4 years before the SRA but must be at least at 63 y/o.
+     That means that we assume 1) everyone qualifies for "Rente für langjährig Versicherte" and 2) that
+     the threshhold for "Rente für langjährig Versicherte" moves with the SRA.
+    """
     return np.maximum(SRA - options["ret_years_before_SRA"], 63)
 
 
 def get_next_period_experience(
-    period, lagged_choice, policy_state, sex, education, experience, informed, options
+    period, lagged_choice, policy_state, sex, education, experience, informed, partner_state, options
 ):
     """Update experience based on lagged choice and period."""
     exp_years_last_period = construct_experience_years(
@@ -186,6 +190,7 @@ def get_next_period_experience(
         education=education,
         policy_state=policy_state,
         informed=informed,
+        partner_state=partner_state,
         options=options,
     )
     # Update if fresh retired
@@ -196,7 +201,7 @@ def get_next_period_experience(
 
 
 def calc_experience_years_for_pension_adjustment(
-    period, sex, experience_years, education, policy_state, informed, options
+    period, sex, experience_years, education, policy_state, informed, partner_state, options
 ):
     """Calculate the reduced experience with early retirement penalty."""
     total_pension_points = calc_total_pension_points(
@@ -217,11 +222,12 @@ def calc_experience_years_for_pension_adjustment(
     early_retirement_penalty_uninformed = options[
         "uninformed_early_retirement_penalty"
     ][education]
+    very_long_insured_test = test_very_long_insured(retirement_age_difference, experience_years, sex, education, partner_state, options)
     early_retirement_penalty = (
         informed * early_retirement_penalty_informed
         + (1 - informed) * early_retirement_penalty_uninformed
     )
-    early_retirement_penalty = 1 - early_retirement_penalty * retirement_age_difference
+    early_retirement_penalty = 1 - early_retirement_penalty * retirement_age_difference*(1-very_long_insured_test)
 
     # Total bonus for late retirement
     late_retirement_bonus = 1 + (
@@ -246,3 +252,16 @@ def calc_experience_years_for_pension_adjustment(
 def construct_experience_years(experience, period, max_exp_diffs_per_period):
     """Experience and period can also be arrays."""
     return experience * (period + max_exp_diffs_per_period[period])
+
+def test_very_long_insured(retirement_age_difference, experience_years, sex, education, partner_state, options):
+    """Test if the individual qualifies for pension for very long insured
+    (Rente besonders für langjährig Versicherte)."""
+    qualified_years = get_qualified_years(experience_years, sex, education, partner_state)
+    enough_years = qualified_years >= 45
+    close_enough_to_SRA = retirement_age_difference <= 2
+    return enough_years * close_enough_to_SRA
+
+def get_qualified_years(experience_years, sex, education, partner_state):
+    """Calculate the qualified years for pension."""
+    is_woman = sex == 1
+    return experience_years + is_woman * 6

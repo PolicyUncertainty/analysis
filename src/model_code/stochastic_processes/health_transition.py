b5351a9c1e15eaf2f18ae52592b09abc85ce51d9
MaxBlesch <maximilianblesch@web.de>
2025-04-21 18:03:08 +0200

    Fixed param name

--- src/model_code/stochastic_processes/health_transition.py
+++ src/model_code/stochastic_processes/health_transition.py
@@ -34,7 +34,7 @@ def calc_disability_probability(params, education, period):
     exp_value = jnp.exp(
         params["disability_logit_const"]
         + params["disability_logit_period"] * period
-        + params["disability_logit_education"] * education
+        + params["disability_logit_high_educ"] * education
     )
     prob = exp_value / (1 + exp_value)
     return prob

7f9e08cca878d1ccdf34912c882517feae85fc44
MaxBlesch <maximilianblesch@web.de>
2025-04-25 16:18:56 +0200

    a100.

--- src/simulation/hu_server_sim.sh
+++ src/simulation/hu_server_sim.sh
@@ -7,8 +7,8 @@
 #SBATCH --ntasks=1
 #SBATCH --time=0:30:00
 #SBATCH --mem=50GB
-#SBATCH --partition=gpu_h100
-#SBATCH --gres=gpu:h100nvl:1
+#SBATCH --partition=gpu_a100
+#SBATCH --gres=gpu:1
 #SBATCH --qos=standard
 
 module add cuda/12.4.1

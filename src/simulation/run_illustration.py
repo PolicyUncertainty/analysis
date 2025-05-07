905ed9f48cd21626cb34ab96f2520e62d1fc1662
gregor-schueler <gregor.schueler.2002@gmail.com>
2025-05-07 19:12:11 +0200

    show fit

--- src/simulation/run_illustration.py
+++ src/simulation/run_illustration.py
@@ -9,7 +9,7 @@ path_dict = create_path_dict()
 specs = generate_derived_and_data_derived_specs(path_dict)
 
 
-model_name = "partner_est"
+model_name = "new"
 load_df = True
 load_solution = True
 load_sim_model = True

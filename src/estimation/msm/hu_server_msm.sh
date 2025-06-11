eca6278e792c83bf3db6de6d9b309a0711800831
MaxBlesch <maximilianblesch@web.de>
2025-06-11 11:35:48 +0200

    HU

---
+++ src/estimation/msm/hu_server_msm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=pol_unc
+#SBATCH --mail-user=maximilian.blesch@hu-berlin.de
+#SBATCH --mail-type=end
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=9:00:00
+#SBATCH --mem=50GB
+#SBATCH --partition=gpu_h100
+#SBATCH --gres=gpu:h100nvl:1
+#SBATCH --qos=standard
+
+module add cuda/12.4.1
+
+python run_model_msm.py

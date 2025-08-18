ba7fb307622900cbf88ace2a3a67122c5bfeaa2e
MaxBlesch <maximilianblesch@web.de>
2025-08-18 17:19:22 +0200

    Changed asset grid

--- src/model_code/wealth_and_budget/assets_grid.py
+++ src/model_code/wealth_and_budget/assets_grid.py
@@ -9,11 +9,21 @@ def create_end_of_period_assets():
     section_4 = np.arange(start=50, stop=100, step=10)  # 3
     section_5 = np.arange(start=100, stop=500, step=100)  # 4
     section_6 = np.arange(start=500, stop=1000, step=200)  # 3
-    # Add 5, 10 and a 100 million to the grid
-    section_7 = np.array([5_000, 10_000, 100_000])
+    # 500_000 to 1000_000 in steps of 200_000
+    section_7 = np.arange(start=1000, stop=10000, step=2000)  # 5
+    # Add 10 and a 100 million to the grid
+    section_8 = np.array([10_000, 100_000])
     savings_grid = np.concatenate(
-        [section_1, section_2, section_3, section_4, section_5, section_6, section_7]
+        [
+            section_1,
+            section_2,
+            section_3,
+            section_4,
+            section_5,
+            section_6,
+            section_7,
+            section_8,
+        ]
     ).astype(float)
     # The steps above are made in thousands, so we multiply by 1000
-    savings_grid = savings_grid * 1000
-    return savings_grid
+    return savings_grid * 1000

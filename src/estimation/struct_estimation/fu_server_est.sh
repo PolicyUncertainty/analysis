b75f8cdcd8a579244f97759d0d6e973a7445215d
MaxBlesch <maximilianblesch@web.de>
2025-09-12 13:35:10 +0200

    Again

--- src/estimation/struct_estimation/fu_server_est.sh
+++ src/estimation/struct_estimation/fu_server_est.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=pol_unc_est
 #SBATCH --mail-user=mblesch@zedat.fu-berlin.de
 #SBATCH --mail-type=end
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --time=7:00:00
+ #SBATCH --nodes=1
+ #SBATCH --ntasks=1
+ #SBATCH --time=7:00:00
 #SBATCH --mem=35GB
-#SBATCH --partition=scavenger
-#SBATCH --gres=gpu:a5000:1
-#SBATCH --qos=standard
+ #SBATCH --partition=scavenger
+ #SBATCH --gres=gpu:a5000:1
+ #SBATCH --qos=standard
 
 module add Python/3.12.3-GCCcore-13.3.0
 module add CUDA/12.0.0
